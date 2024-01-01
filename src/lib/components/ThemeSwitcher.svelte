6de869c5eab2db0bf6ca87685cdb17c3e9243e2b
Noam Shemesh <noams@hey.com>
2024-01-01 09:03:28 +0200

    add shadcn

--- src/lib/components/ThemeSwitcher.svelte
+++ src/lib/components/ThemeSwitcher.svelte
@@ -1,65 +1,64 @@
 <script lang="ts">
+	// Utils
 	import type { SubmitFunction } from '@sveltejs/kit';
 	import { enhance } from '$app/forms';
-	import { page } from '$app/stores';
 	import { themes } from '$lib/utils/themes';
+	import { browser } from '$app/environment';
 
-	let preferred_theme: string = $page.data.theme ?? themes.dark;
+	// Stores
+	import { page } from '$app/stores';
 
-	const handle_set_theme_submit: SubmitFunction = ({ action }) => {
+	// Components
+	import { Button } from '$components/ui/button';
+
+	// Icons
+	import { Sun, Moon } from 'radix-icons-svelte';
+
+	let preferredTheme: string = $page.data.theme;
+
+	if (browser && !document.cookie.includes('theme')) {
+		let prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+		preferredTheme = prefersDark.matches ? themes.dark : themes.light;
+
+		prefersDark.addEventListener('change', (e) => {
+			preferredTheme = e.matches ? themes.dark : themes.light;
+		});
+	}
+
+	const handleSetTheme: SubmitFunction = ({ action }) => {
 		const theme = action.searchParams.get('theme');
 
 		if (theme) {
-			preferred_theme = theme;
-			document.documentElement.setAttribute('data-theme', theme);
+			preferredTheme = theme;
+			document.documentElement.setAttribute('class', theme);
 		}
 	};
 </script>
 
-<form method="POST" use:enhance={handle_set_theme_submit}>
-	{#if preferred_theme === themes.dark}
-		<button
+<form method="POST" use:enhance={handleSetTheme}>
+	{#if preferredTheme === themes.dark}
+		<Button
 			id="light-theme-switcher"
+			type="submit"
 			aria-label="Switch to light theme"
-			formaction="/?/set_theme&theme={themes.light}&redirect_to={$page.url.pathname}"
-			class="btn btn-ghost rounded-l-md rounded-r-none bg-base-200"
+			formaction="/theme?/set&theme={themes.light}&redirect={$page.url.pathname}"
+			variant="outline"
+			size="icon"
+			class="rounded-r-none border-r-0"
 		>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke-width="1.5"
-				stroke="currentColor"
-				class="h-6 w-6"
-			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
-				/>
-			</svg>
-		</button>
+			<Sun />
+		</Button>
 	{:else}
-		<button
+		<Button
 			id="dark-theme-switcher"
+			type="submit"
 			aria-label="Switch to dark theme"
-			formaction="/?/set_theme&theme={themes.dark}&redirect_to={$page.url.pathname}"
-			class="btn btn-ghost rounded-l-md rounded-r-none bg-base-200"
+			formaction="/theme?/set&theme={themes.dark}&redirect={$page.url.pathname}"
+			variant="outline"
+			size="icon"
+			class="rounded-r-none border-r-0"
 		>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke-width="1.5"
-				stroke="currentColor"
-				class="h-6 w-6"
-			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
-				/>
-			</svg>
-		</button>
+			<Moon />
+		</Button>
 	{/if}
 </form>
--- src/routes/(config)/theme/+page.server.ts
+++ src/routes/(config)/theme/+page.server.ts
@@ -1,17 +1,18 @@
 import { redirect } from '@sveltejs/kit';
 
 export const actions = {
-	set_theme: async ({ url, cookies }) => {
+	set: async ({ url, cookies }) => {
 		const theme = url.searchParams.get('theme');
-		const redirect_to = url.searchParams.get('redirect_to');
+		const redirectTo = url.searchParams.get('redirect');
 
 		if (theme) {
 			cookies.set('theme', theme, {
 				path: '/',
+				httpOnly: false,
 				maxAge: 60 * 60 * 24 * 365
 			});
 		}
 
-		redirect(303, redirect_to ?? '/');
+		redirect(303, redirectTo ?? '/');
 	}
 };
