5a8063bf504ec0a8152d99ecb6fc54d647804029
Noam Shemesh <noams@hey.com>
2024-03-17 08:28:39 +0200

    🗑️cleanup

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,8 @@
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', '.svelte.md', '.md', '.svx'],
-
 	kit: {
 		adapter: adapter(),
 
@@ -15,15 +12,7 @@ const config = {
 		}
 	},
 
-	preprocess: [
-		vitePreprocess(),
-		mdsvex({
-			extensions: ['.svelte.md', '.md', '.svx'],
-			smartypants: {
-				dashes: 'oldschool'
-			}
-		})
-	]
+	preprocess: [vitePreprocess()]
 };
 
 export default config;
