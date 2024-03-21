0e0904a193c2f53c438db015d16d07f5c0fff5e3
Noam Shemesh <noams@hey.com>
2024-03-21 10:04:15 +0200

    🚧WIP

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,25 +1,4 @@
-import { dev } from '$app/environment';
-import { logsnag } from '$lib/server/logsnag';
-
 export const load = async (event) => {
-	if (!dev) {
-		const ip = event.getClientAddress();
-		try {
-			await logsnag.track({
-				channel: 'visits',
-				event: 'New Visit',
-				description: `New visit from ${ip}`,
-				icon: '👀',
-				notify: true,
-				tags: {
-					ip: ip
-				}
-			});
-		} catch (error) {
-			console.log(error);
-		}
-	}
-
 	const theme = event.locals.theme;
 	return {
 		theme: theme ?? null
