c5d640019b17a534c48b7c7f5822448c960e994e
Noam Shemesh <noams@hey.com>
2024-08-14 19:34:28 +0300

    🩹remove filtering

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,23 +2,11 @@ import { redirect } from '@sveltejs/kit';
 import { projects } from '$lib/data/projects';
 
 export const load = async ({ url }) => {
-	const filter = url.searchParams.get('filter');
-
-	const filtered_projects = projects.filter((project) => {
-		if (filter) {
-			if (project.type === filter) {
-				return project;
-			}
-		} else {
-			return project;
-		}
-	});
-
 	if (projects.length === 0) {
-		redirect(301, '/projects');
+		redirect(301, '/');
 	}
 
 	return {
-		projects: filtered_projects
+		projects
 	};
 };
